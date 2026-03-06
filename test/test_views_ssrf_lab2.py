d3cf8c30b5805e6aec83fcd99bb5e79a45bd97ee
RohasRedz <32270188+RohasRedz@users.noreply.github.com>
2026-03-06 14:38:09 +0530

    Add delta unit test for introduction/views.py

--- test/test_views_ssrf_lab2.py
+++ test/test_views_ssrf_lab2.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 from introduction import views
 
 
@@ -8,7 +7,7 @@ def test_ssrf_lab2_ignores_user_supplied_url_and_uses_safe_endpoint(mocker):
     request = mocker.Mock()
     request.user.is_authenticated = True
     request.method = "POST"
-    request.POST.__getitem__.return_value = "http://169.254.169.254/latest/meta-data"  # should be ignored
+    request.POST.__getitem__.return_value = "http://169.254.169.254/latest/meta-data"
 
     get_mock = mocker.patch("introduction.views.requests.get", return_value=mocker.Mock(content=b"OK"))
     render_mock = mocker.patch("introduction.views.render", return_value="rendered")
